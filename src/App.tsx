a23a097e021413ff5b8f9e627aa10cdef7498f57
Aleksandr Viznyi <senak98@yandex.ru>
2024-03-01 14:03:55 +0700

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 // import { useState } from "react";
-
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 import "./App.css";
+import Header from "./components/Header/Header.jsx";
 
 function App() {
-
-  return <div className="App"></div>;
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <Header />
+      </div>
+    </Provider>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,71 @@
+import "./Header.scss";
+import logo from "../../images/panda_logo.jpg";
+import { Select } from "antd";
+import { MailOutlined } from "@ant-design/icons";
+import { useWindowWidth } from "../../hooks/useWindowWidth.ts";
+import tg from "../../images/tg.svg";
+
+function Header() {
+  const windowWidth = useWindowWidth();
+
+  return (
+    <>
+      <header className="header">
+        <div className="header__wrapper">
+          <div className="header__logo"><img className="header__img" src={logo} alt="easypandamoney logo" /></div>
+          <h2 className="header__title">Easy Panda Money</h2>
+        </div>
+        <div className="header__container">
+          <ul className="header__list">
+            {windowWidth <= 700 ? (
+              <li className="header__contact">
+                <a
+                  className="header__link"
+                  href="mailto:easypanda247@gmail.com"
+                >
+                  <MailOutlined className="header__contact_tg-img" />
+                </a>
+              </li>
+            ) : (
+              <a className="header__link" href="mailto:easypanda247@gmail.com">
+                <li className="header__contact">easypanda247@gmail.com</li>
+              </a>
+            )}
+            {windowWidth <= 700 ? (
+              <li className="header__contact">
+                <a
+                  className="header__link"
+                  href="tg://resolve?domain=easypandamoney/"
+                >
+                  <img
+                    className="header__contact_tg-img"
+                    src={tg}
+                    alt="Написать в Telegram"
+                  />
+                </a>
+              </li>
+            ) : (
+              <li className="header__contact">
+                <a
+                  className="header__link"
+                  href="tg://resolve?domain=easypandamoney/"
+                >
+                  Написать в Telegram
+                </a>
+              </li>
+            )}
+          </ul>
+          <Select
+            defaultValue={"ru"}
+            options={[
+              { value: "ru", label: "RU" },
+              { value: "eng", label: "ENG" },
+            ]}
+          ></Select>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
