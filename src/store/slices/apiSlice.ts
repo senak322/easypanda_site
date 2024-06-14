b5ac9c4bd526033a6d02cfb309aaf4bc725dc215
Aleksandr Viznyi <senak98@yandex.ru>
2024-06-14 22:29:19 +0300

    fix api

--- src/store/slices/apiSlice.ts
+++ src/store/slices/apiSlice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // debugger
 export const apiSlice = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3001" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://easypandamoney.com/api" }),
   endpoints: (builder) => ({
     createOrder: builder.mutation<any, FormData>({
       query: (formData) => ({
